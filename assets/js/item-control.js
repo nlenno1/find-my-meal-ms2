06ff03606ce781b1cb13188d6aac77a7c35b37a5
nlenno1 <nick.lennon@hotmail.co.uk>
2021-05-08 21:27:41 +0000

    feat: added Clear My Supplies button to all My Supplies Modals with the functionality to remove objects and clear Local Storage

--- assets/js/item-control.js
+++ assets/js/item-control.js
@@ -2,87 +2,131 @@
 
 // defining all global variables 
 const regex = new RegExp(/^[a-zA-Z]+$/);
+let zeroWasteIngredientsArray = []
 
 // open array saved in local storage and display on console
-let mySupplesArray = JSON.parse(localStorage.getItem("mySuppliesIngredientList"));
-console.log((mySupplesArray == undefined ? "No instance of Supplies Array Exists" : "Supplies Array Exists"))
-console.log("My Supplies Array opened from Local Storage : " + (mySupplesArray == "" ? "No items to load!" : mySupplesArray));
-// turn item in the array into objects
-for (item of mySupplesArray) {
-    createIngredientObject (item, $("#my-supplies-display"))
-} 
+let mySuppliesArray = JSON.parse(localStorage.getItem("mySuppliesSavedList"));
+console.log((mySuppliesArray == undefined || null ? "No instance of My Supplies Array Exists" : "My Supplies Array Exists"))
+console.log("My Supplies Array opened from Local Storage : " + (mySuppliesArray == "" ? "No items to load!" : mySuppliesArray));
+// avoid error with null value
+mySuppliesArray = (mySuppliesArray == null)? []: mySuppliesArray;
 
-// add button click events
+// turn item in the array into objects
+for (item of mySuppliesArray) {
+    createIngredientObject (item, $("#my-supplies-display"));
+};
+
+
+// ADD BUTTON AND ENTER KEY EVENTS
 // my supplies input add button 
 $("#supplies-add-button").click(function() {
-    addItemToDisplay();
+    addItemToDisplay($("#ingredient-name").val().toLowerCase(), "#my-supplies-display", "#ingredient-name", mySuppliesArray, "My Supplies", "mySuppliesArray");
 });
 
-// enter key event 
+// my supplies input enter key event 
 $("#ingredient-name").keypress(function(event) {
     let key = event.keyCode;
     if (key == "13") {
-        addItemToDisplay ();
+        addItemToDisplay ($("#ingredient-name").val().toLowerCase(), "#my-supplies-display", "#ingredient-name", mySuppliesArray, "My Supplies", "mySuppliesArray");
     }
 });
 
-function createIngredientObject (ingredient, area) {
-    // add new element to designated display area 
-    $(".my-supplies-display").append(
-        `<div class="ingredient-added" id="${ingredient}-supplies-item" value="${ingredient}"> 
-            <p> ${capitalizeFirstLetter(ingredient)} </p>
-            <button type="button" id="${ingredient}-remove-button" class="btn-close" aria-label="Remove Item"></button>
-        </div>`
-    );
-    // object animations
-    $("#" + ingredient + "-supplies-item").hide();
-    $("#" + ingredient + "-supplies-item").slideDown();
+// zero-waste input add button 
+$("#zero-waste-add-button").click(function() {
+    addItemToDisplay($("#zero-waste-ingredient-name").val().toLowerCase(), "#zero-waste-ingredients-display", "#zero-waste-ingredient-name", zeroWasteIngredientsArray, "Zero-Waste Ingredients", "zeroWasteIngredientsArray");
+});
 
-    // remove item functionality 
-    $("#" + ingredient + "-remove-button").click(function() {
-        item = $(this).parent();
-        removeItemFromArray(mySupplesArray, item.attr("value"))
-        console.log(item.attr("value") + ' removed from "My Supplies"')
-        console.log(mySupplesArray)
-        saveSuppliesToLocalDrive()
-        item.fadeOut(function() {
-            item.remove();
-        });
-    });
-};
+// zero-waste input enter key event 
+$("#zero-waste-ingredient-name").keypress(function(event) {
+    let key = event.keyCode;
+    if (key == "13") {
+        addItemToDisplay ($("#zero-waste-ingredient-name").val().toLowerCase(), "#zero-waste-ingredients-display", "#zero-waste-ingredient-name", zeroWasteIngredientsArray, "Zero-Waste Ingredients", "zeroWasteIngredientsArray");
+    }
+});
 
-// Add item button functionality 
-function addItemToDisplay () {
-    //store input value as variable in the correct format 
-    let ingredient = $("#ingredient-name").val().toLowerCase()
+// clear My Supplies click event
+$("#clear-my-supplies-button").click(function() {
+    clearLocalStorge();
+});
+
+// Add item functionality 
+function addItemToDisplay (ingredient, targetArea, inputArea, arrayToAction, arrayName, compArrayName) {
     // clear input value for next ingredient and keep focus on input box 
-    $("#ingredient-name").val("");
-    $("#ingredient-name").focus();
+    focusAndClear(inputArea);
     
     // control conditional to avoid adding a empty value 
     if (ingredient.length == 0) {
         // empty value user feedback 
         alert("Please enter an ingredient name");
     } else if (!regex.test(ingredient)) {
+        focusAndClear(inputArea);
         alert("Please only use letters in the ingredient name");
-    } else if (mySupplesArray.includes(ingredient)) {
+    } else if (arrayToAction.includes(ingredient)) {
         // duplicate value user feedback 
-        alert(capitalizeFirstLetter(ingredient) + " has already been added");
+        focusAndClear(inputArea);
+        alert(capitalizeFirstLetter(ingredient) + " has already been added to " + arrayName);
     } else {
         // add user input to supplies array 
-        mySupplesArray.push(ingredient);
-        saveSuppliesToLocalDrive();
+        arrayToAction.push(ingredient);
+        arrayToAction == "mySuppliesArray"? saveSuppliesToLocalDrive():
         // create Ingredient Object
-        createIngredientObject (ingredient)
+        createIngredientObject (ingredient, targetArea, arrayName, compArrayName)
+        // remove item functionality 
+        removeObjectMethod(ingredient, targetArea, inputArea, arrayToAction, arrayName, compArrayName)
         // show completion of task on log
-        console.log(ingredient + ' added to "My Supplies"')
-        console.log("My Supplies Array : " + mySupplesArray);
+        console.log(ingredient + ' added to ' + arrayName)
+        console.log(arrayName + " : " + arrayToAction);
     }
 };
 
+function createIngredientObject (ingredient, targetArea, arrayName, compArrayName) {
+    // add new element to designated display area 
+    $(targetArea).append(
+        `<div class="ingredient-added" id="${ingredient}-in-${compArrayName}" value="${ingredient}"> 
+            <p> ${capitalizeFirstLetter(ingredient)} </p>
+            <button type="button" id="${ingredient}-in-${compArrayName}-remove-button" class="btn-close" aria-label="Remove Item"></button>
+        </div>`
+    );
+    // object animations
+    $("#" + ingredient + "-supplies-item").hide();
+    $("#" + ingredient + "-supplies-item").slideDown();
+
+    
+};
+
+function removeObjectMethod(ingredient, targetArea, inputArea, arrayToAction, arrayName, compArrayName) {
+    $(`#${ingredient}-in-${compArrayName}-remove-button`).click(function () {
+        item = $(this).parent();
+        removeItemFromArray(arrayToAction, item.attr("value"));
+        console.log(item.attr("value") + ' removed from ' + arrayName);
+        console.log(arrayToAction);
+        arrayToAction == "mySuppliesArray"? saveSuppliesToLocalDrive():
+        item.fadeOut(function () {
+            item.remove();
+        });
+    });
+}
+
+function focusAndClear(targetInput) {
+    $(targetInput).val("");
+    $(targetInput).focus();
+};
+
+function clearLocalStorge() {
+    for (name of mySuppliesArray) {
+        item = $(`#${name}-in-mySuppliesArray`)
+        console.log(name + " removed from mySuppliesArray")
+        item.fadeOut(function () {
+            item.remove();
+        });
+    }
+    localStorage.setItem("mySuppliesSavedList", JSON.stringify([]));
+    console.log("Local Storage array cleared")
+}
+
 
 function saveSuppliesToLocalDrive() {
-    localStorage.setItem("mySuppliesIngredientList", JSON.stringify(mySupplesArray));
+    localStorage.setItem("mySuppliesSavedList", JSON.stringify(mySuppliesArray));
 }
 
 function capitalizeFirstLetter(string) {
