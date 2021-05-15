15b4e0cc66f48c2b61349d4e7e919429b4b3d6d3
nlenno1 <nick.lennon@hotmail.co.uk>
2021-05-15 18:36:11 +0000

    feat: added loading gif to sending email feedback

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -3,6 +3,7 @@ console.log("emailJS linked")
 function sendMail(contactForm) {
     console.log("Sending Email")
     $("#find-my-meal-button").val("Sending email...");
+    $("#loading").html('<img src="assets/images/loading.gif">')
     emailjs.send("nlenno1_email", "fmm_emailjs_template", {
         from_name: contactForm.name.value,
         from_email: contactForm.email.value,
@@ -14,10 +15,12 @@ function sendMail(contactForm) {
             alert("Your message has been sent")
             $("input").val("");
             $("textarea").val("");
+            $("#loading").html('')
             $("#find-my-meal-button").val("Send Email");
         },
         function(err) {
             console.log("Sending Failed", err)
+            $("#loading").html('')
             alert("There was an error sending your message", err.message)
         }
     );
