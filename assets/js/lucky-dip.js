de456a86a1927a737b5c05569668b16631879825
nlenno1 <nick.lennon@hotmail.co.uk>
2021-05-18 18:10:14 +0000

    feat: added Lucky Dip API call to Show Me Another Button click event

--- assets/js/lucky-dip.js
+++ assets/js/lucky-dip.js
@@ -1,5 +1,13 @@
 $(window).ready (function() {
-    url = "https://api.spoonacular.com/recipes/random?apiKey=c4a7c11521de4bae8f06ba9fd8e9ac17&limitLicense=true&number=1"
-    console.log(url)
-    makeApiCall (url, "single-recipe-to-display")
+    luckyDipApiCall();
 });
+
+$("#lucky-dip-button").click(function() {
+    luckyDipApiCall()
+});
+
+function luckyDipApiCall() {
+    url = "https://api.spoonacular.com/recipes/random?apiKey=c4a7c11521de4bae8f06ba9fd8e9ac17&limitLicense=true&number=1";
+    console.log("Making API call to: ", url);
+    makeApiCall(url, "single-recipe-to-display");
+}
