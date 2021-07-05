33ccbb13b427cc69f75e465093d5ab3a9f773c14
nlenno1 <nick.lennon@hotmail.co.uk>
2021-07-05 18:32:17 +0000

    doc: added comments to load-results.js

--- assets/js/load-results.js
+++ assets/js/load-results.js
@@ -2,16 +2,17 @@
 $(window).ready (function() {
     loadStoredResults();
 });
-
+//function to load results stored in local storage
 function loadStoredResults() {
+    //conditional statement to check if load is needed
     if (loadFromLocalStorage("reloadResults") == true) {
-        console.log("Reloading results");
+        //initialize variable and store loaded results in it
         let searchResults = loadFromLocalStorage("latestSearchResults");
-        console.log(searchResults);
+        //display the results that have been loaded
         displaySearchResults(searchResults, loadFromLocalStorage("backToResultsPageToLoad"));
+        //clear data under backToResultsPageToLoad tag in local storage
         saveToLocalStorage("", "backToResultsPageToLoad");
+        //disable load results instruction to stop the results being loaded again if user navigates away and back to page
         disableLoadStoredResults();
-    } else {
-        console.log("No previous results to load");
     }
 }
