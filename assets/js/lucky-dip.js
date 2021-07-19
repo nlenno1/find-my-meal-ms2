a24ab1edfbc7e043710b975617ba30310d05e949
nlenno1 <nick.lennon@hotmail.co.uk>
2021-07-19 14:58:52 +0000

    removed ALL CONSOLE.LOG() statments

--- assets/js/lucky-dip.js
+++ assets/js/lucky-dip.js
@@ -11,8 +11,6 @@ $("#lucky-dip-button").click(function() {
 function luckyDipApiCall() {
     //storing the required url as a variable
     let url = "https://api.spoonacular.com/recipes/random?apiKey=c4a7c11521de4bae8f06ba9fd8e9ac17&limitLicense=true&number=1";
-    //developer feedback
-    console.log("Making API call to: ", url);
     //calling the makeApiCall function and passing in the URL and searchType
     makeApiCall(url, "single-recipe-to-display");
     // delay to allow the results to load into the page before scrolling back to the top if needed for UX
