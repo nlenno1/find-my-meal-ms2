222a3f52941f115396ceeab64bba2e2ba572de02
nlenno1 <nick.lennon@hotmail.co.uk>
2021-07-20 21:49:55 +0000

    fix: fixed tablet/mobile footer spacing to the bottom of the page issue

--- assets/js/footer-position.js
+++ assets/js/footer-position.js
@@ -1,14 +1,23 @@
-// Window ready function to disble the loading process for the "Back to results" button.
+// Window ready function to call function when window is loaded
 $(window).ready (function() {
     footerPosition();
 })
-
+// Window resize listener to call function when the window is resized
 $(window).resize (function() {
     footerPosition();
 })
-
+// Window scroll event listener to call function when window is scrolled
+$(window).scroll (function() {
+    footerPosition();
+})
+// defining footerPosition function to change the footer position depending on screen height
 function footerPosition() {
-    console.log(($(window).width()));
-    console.log($( window ).outerHeight());
-    $(".background-image").css("min-height", `calc(${$( window ).outerHeight()} - 184px)`);
+    // conditional statement to check height of background image and footer against the window height
+    if ($(".background-image-container").outerHeight() + $("footer").outerHeight() < $( window ).height()) {
+        // change footer css to fix the footer to the bottom of the window
+        $("footer").css("position", "fixed")
+    } else if  ($(".background-image-container").outerHeight() + $("footer").outerHeight() > $( window ).height()) {
+        //change footer css to have it after all the other page elements
+        $("footer").css("position", "relative")
+    }
 };
